60289a39fe48e188d79477664705879b192903ea
chik <ochkurov96@gmail.com>
2023-10-28 20:01:05 +0300

    Initial commit

---
+++ src/features/cards/componets/Cards/Cards.tsx
@@ -0,0 +1,12 @@
+import {useParams} from "react-router-dom";
+
+export const Cards = () => {
+let {packId} = useParams<{ packId:string }>();
+	console.log('packId', packId)
+	return (
+		<div>
+
+			<h1>Cards</h1>
+		</div>
+	);
+};
