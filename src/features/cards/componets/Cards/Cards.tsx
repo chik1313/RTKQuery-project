27e1e0f1e64383313c0e0bf10530f784035fb82d
chik <ochkurov96@gmail.com>
2023-10-30 16:28:33 +0200

    update project add post and get methods

--- src/features/cards/componets/Cards/Cards.tsx
+++ src/features/cards/componets/Cards/Cards.tsx
@@ -1,12 +1,72 @@
 import {useParams} from "react-router-dom";
+import {useAddCardMutation, useGetCardsQuery} from "../../service/cards.api";
+import LinearProgress from "@mui/material/LinearProgress";
+import {nanoid} from "@reduxjs/toolkit";
+import {ArgCreateCardType} from "../../service/types";
+import {toast} from "react-toastify";
+import {ChangeEvent} from "react";
+import {Pagination} from "@mui/material";
+import s from "./styles.module.css"
 
 export const Cards = () => {
-let {packId} = useParams<{ packId:string }>();
-	console.log('packId', packId)
-	return (
-		<div>
+    let {packId} = useParams<{ packId: string }>();
 
-			<h1>Cards</h1>
-		</div>
-	);
+    const {data, isLoading, isError, error, refetch} = useGetCardsQuery(packId ?? "");
+
+    const [addCard] = useAddCardMutation()
+
+    if (isLoading) return <LinearProgress color={"secondary"}/>
+
+    if (isError) {
+        const err = error as any
+        return <h1>{err.data.error}</h1>
+    }
+    const addCardHandler = () => {
+        if (packId) {
+            const newCard: ArgCreateCardType = {
+                cardsPack_id: packId,
+                question: "🐱 question " + nanoid(),
+                answer: "🐙 answer " + nanoid(),
+            };
+            addCard(newCard)
+                .unwrap()
+                .then((res) => {
+                    const cardQuestion = res.newCard.question;
+                    toast.success(`Карточка ${cardQuestion} успешно добавлена`);
+                })
+                .catch((err)=> {
+                    toast.error(err.data.error)
+                })
+        }
+    }
+
+    const changePageHandler = (event: ChangeEvent<unknown>, page: number) => {
+        console.log("page: ", page);
+    };
+
+    return (
+        <div>
+            <h1>Cards</h1>
+            <button onClick={addCardHandler}>add card</button>
+            <button onClick={refetch}>get new data</button>
+            <div>
+                {
+                    data?.cards.map((card) => {
+                        return (
+                            <div className={s.container} key={card._id}>
+                                <div>
+                                    <b>Question: </b>
+                                    <p>{card.question}</p>{" "}
+                                </div>
+                                <div>
+                                    <b>Answer: </b>
+                                    <p>{card.answer}</p>{" "}
+                                </div>
+                            </div>
+                        );
+                    })}
+            </div>
+            <Pagination count={data?.cardsTotalCount} onChange={changePageHandler} />
+        </div>
+    );
 };
--- src/features/cards/service/cards.api.ts
+++ src/features/cards/service/cards.api.ts
@@ -1,22 +1,40 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import {baseURL} from "../../../common/api/common.api";
+import {AddCardResponseType, ArgCreateCardType, FetchCardsResponseType} from "./types";
 
 export const cardsApi = createApi({
-    reducerPath:"cardsApi",
-    baseQuery: fetchBaseQuery({baseUrl:baseURL}),
+    reducerPath: "cardsApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: baseURL,
+        credentials: "include"
+    }),
+    tagTypes: ["Card"],
     endpoints: (build) => {
         return {
-            getCards: build.query<any, string>({
+            getCards: build.query<FetchCardsResponseType, string>({
                 query: (packId) => {
                     return {
-                        method: "GET",
                         url: "cards/card",
                         params: {
                             cardsPack_id: packId,
                         },
                     };
                 },
+                providesTags: ['Card']
             }),
+            addCard: build.mutation<AddCardResponseType, ArgCreateCardType>({
+                query: (card) => {
+                    return {
+                        method: "POST",
+                        url: "cards/card",
+                        body: {
+                            card
+                        },
+                    }
+                }
+            })
         };
     },
 })
+
+export const { useGetCardsQuery, useAddCardMutation} = cardsApi
