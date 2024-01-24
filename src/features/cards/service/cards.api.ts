393d9f0770fca30ce29f3e072db38522819c8a09
chik <ochkurov96@gmail.com>
2024-01-24 19:14:00 +0200

    update project

--- src/features/cards/service/cards.api.ts
+++ src/features/cards/service/cards.api.ts
@@ -44,7 +44,7 @@ export const cardsApi = createApi({
                 query: (id) => {
                     return {
                         method: "DELETE",
-                        url: "cards/card",
+                        url: "cardыыыыыs/card",
                         params: {
                             id,
                         },
