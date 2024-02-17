8d1cc920c859c90d02c9c0e474b13693e4446ddb
chik <ochkurov96@gmail.com>
2024-02-17 20:37:53 +0200

    update project

--- src/features/packs/packs.api.ts
+++ src/features/packs/packs.api.ts
@@ -2,7 +2,7 @@ import { instance } from "common/api/common.api";
 
 export const packsApi = {
 	getPacks: () => {
-		return instance.get<FetchPacksResponseType>("cardssss/pack", {
+		return instance.get<FetchPacksResponseType>("cards/pack", {
 			params: {
 				//❗Ваш user_id
 				user_id: "6435620aaf58963e887fb0f4",
