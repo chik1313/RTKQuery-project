438d1ea7016f7691668ca49e558fcbe866dbb697
chik <ochkurov96@gmail.com>
2024-03-04 21:23:26 +0200

    add button

--- src/features/packs/packs.api.ts
+++ src/features/packs/packs.api.ts
@@ -11,7 +11,7 @@ export const packsApi = {
 		});
 	},
   createPack: (cardsPack: ArgCreatePackType) => {
-    return instance.post<CreatePackResponseType>("cardsaaaa/pack", { cardsPack });
+    return instance.post<CreatePackResponseType>("cards/pack", { cardsPack });
   },
   removePack: (id: string) => {
     return instance.delete<RemovePackResponseType>(`cards/pack?id=${id}`);
